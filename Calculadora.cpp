ab65770644722192b9ea0d51cc93c3182c02fe22
José de Jesús Ochoa Magallanes <120300330+Ghost-118@users.noreply.github.com>
2026-09-25 17:38:37 -0600

    Add comments to Calculadora.cpp
    Added comments for better code clarity.

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -4,25 +4,30 @@ using namespace std;
 
 int main () {
 	
+	// Declaración de variables
 	int N1, N2, suma, resta, multiplicacion, division;
 	char name[10];
 	
+	// Lectura del nombre del usuario
 	cout<<"Hola, Usuario"<<endl;
 	cout<<"Ingresa tu nombre: "<<endl; 
 	cin>>name;
 	cout<<"Hola: "<<name<<endl; 
 	
+	// Lectura de los números
 	cout<<"Ingresa un numero: "<<endl;
 	cin>>N1; 
 	
 	cout<<"Ingresa un numero: "<<endl;
 	cin>>N2; 
 	
+	// Operaciones matemáticas
 	suma = N1 + N2;
 	resta = N1 - N2;
 	multiplicacion = N1 * N2;
 	division = N1 / N2;
 	
+	// Impresión de resultados
 	cout<<"El resultado de la suma es: "<<suma<<endl;
 	cout<<"El resultado de la resta es: "<<resta<<endl;
 	cout<<"El resultado de la multiplicacion es: "<<multiplicacion<<endl;
